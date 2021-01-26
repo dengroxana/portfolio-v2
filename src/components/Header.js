48ae9752bee434d51c17a8180b6e12ed3f498471
Roxana Deng <roxanadeng@Roxanas-Air.fios-router.home>
2021-01-25 22:20:01 -0500

    added background image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Header.css";
+import bridge from "../assets/brooklyn-bridge.jpg"
 
 
 function Header() {
   return (
     <div id="Header">
+      {/* <img src={bridge} alt="" className="bridge"/> */}
       <div className="header-container">
         <h1>
           Hello! My name is Roxana & I'm a <span className="title">Software Engineer</span> based in NYC.
